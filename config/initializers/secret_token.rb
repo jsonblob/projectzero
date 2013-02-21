ec0f704299d0d15c0a373b97e506e479592f3b98
mjhu <mjhu91@gmail.com>
2013-02-20 20:10:52 -0500

    Created app for Project Zero

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '63719f32aab90c0393d99242878395bbe54d36c37939e4e35a362184a01e7ba75b998580b627a08ae793a37c874e39bf063c0dfe3f61739c95538383c93cefcc'
